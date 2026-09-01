6a9a5aa60d649bf82c58af2301104969ff34e6fb
elmraniyoussef <elmraniyoussef19@gmail.com>
2026-09-01 11:01:12 +0200

    first

---
+++ test.js
@@ -0,0 +1,11 @@
+const player_name = "youssef";
+let level = 100;
+let age = 28;
+let score = "max";
+const country = "moroco";
+
+console.log("hello " + player_name );
+console.log("your age is " + age);
+console.log("you've recheed level " + level);
+console.log("with a " + score+" score ");
+console.log("in " + country);
