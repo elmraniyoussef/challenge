0c2ac51490f2f3c9c3d9b0f71058c7b0888904cf
elmraniyoussef <elmraniyoussef19@gmail.com>
2026-09-01 17:54:40 +0200

    hello

---
+++ chalanges variables/challeng 6.js
@@ -0,0 +1,9 @@
+let scorepartie1 = 10
+let scorepartie2 = 20
+let scorepartie3 = 30
+let scorepartie4 = 40
+let scoretotal = (scorepartie1 + scorepartie2 + scorepartie3 + scorepartie4);
+let scoremoyenne =( scoretotal / 4);
+console.log("score total: " + scoretotal);
+console.log("score moyenne: " + scoremoyenne)
+
